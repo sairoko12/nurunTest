36cc9ab0d56a3e7a25574961eabc629e5605b0e1
Cristian Benavides Jimenez <sairoko16@gmail.com>
2015-04-30 13:01:41 -0500

    Validacion de tiempo agregada

--- sql/nurun_test.sql
+++ sql/nurun_test.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50541
 File Encoding         : 65001
 
-Date: 2015-04-30 11:35:50
+Date: 2015-04-30 13:01:17
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -27,7 +27,7 @@ CREATE TABLE `cifras` (
   PRIMARY KEY (`id_cifra`),
   KEY `fk_cifra_usuario` (`id_usuario`),
   CONSTRAINT `fk_cifra_usuario` FOREIGN KEY (`id_usuario`) REFERENCES `usuarios` (`id_usuario`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
 
 -- ----------------------------
 -- Table structure for usuarios
@@ -37,10 +37,11 @@ CREATE TABLE `usuarios` (
   `id_usuario` int(25) NOT NULL AUTO_INCREMENT,
   `visible_id` varchar(10) NOT NULL,
   `id_facebook` varchar(55) NOT NULL,
-  `nombre` varchar(100) NOT NULL,
+  `username` varchar(50) DEFAULT NULL,
+  `nombre` varchar(100) DEFAULT NULL,
   `email` varchar(50) DEFAULT NULL,
   `original_picture` varchar(255) DEFAULT NULL,
   `app_picture` varchar(155) DEFAULT NULL,
   `status` tinyint(1) NOT NULL DEFAULT '1',
   PRIMARY KEY (`id_usuario`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
